3d69d53eecebdaee493f7c91ec44ca03b63a6fa2
Tanmay9724349 <156205714+Tanmay9724349@users.noreply.github.com>
2024-03-31 14:05:33 +0000

    Main

--- Backend/schema.js
+++ Backend/schema.js
@@ -1,4 +1,15 @@
 const mongoose = require('mongoose');
-const schema = newmongoose.schema({
-    
+const schema = new mongoose.Schema({
+    Name:{type:String,require},
+    Email:{type:String,require},
+    Phonenumber:{type:Number,require},
+    Age:{type:Number,require},
+    Gender:{type:String,require},
+    IncidentDate:{type:String,require},
+    IncidentLocation:{type:String,require},
+    IncidentDescription:{type:String,require},
+    Harassment:{type:String,require},
+    Other:{type:String,require},
+    Acknolegment:{type:String,require}
 })
+module.exports = mongoose.model('report',schema);
